bafd4e0bda92fa3de7812cf70ebc7b10f17319b7
pxx917144686 <111606963+pxx917144686@users.noreply.github.com>
2024-12-29 23:34:50 +0800

    Add files via upload

--- worker/decode.js
+++ worker/decode.js
@@ -1,5 +1,3 @@
----
----
 /**
  * @name  {{ site.name }}
  * @description  {{ site.description }}
@@ -13,8 +11,12 @@
 /* eslint-disable no-console */
 
 self.addEventListener('message', (e) => {
-  self.importScripts('{{ "third_party/mathjs/math.min.js" | relative_url }}');
-  self.importScripts('{{ "lib/utils.js" | relative_url }}');
+  try {
+    self.importScripts('{{ "third_party/mathjs/math.min.js" | relative_url }}');
+    self.importScripts('{{ "lib/utils.js" | relative_url }}');
+  } catch (err) {
+    throw new Error('原因：加载脚本失败、加载依赖失败');
+  }
 
   let source = e.data.source;
   const packer = e.data.packer;
@@ -47,6 +49,7 @@ self.addEventListener('message', (e) => {
     },
     aaencode: () => {
       self.importScripts('{{ "third_party/cat-in-136/aadecode.js" | relative_url }}');
+      if (!AADecode.detect(source)) throw '原因：给定的代码不是以 aaencode 编码的';
       return AADecode.decode(source);
     },
     jjencode: () => {
@@ -56,22 +59,22 @@ self.addEventListener('message', (e) => {
     urlencode: () => {
       self.importScripts('{{ "third_party/js-beautify/unpackers/urlencode_unpacker.js" | relative_url }}');
       if (Urlencoded.detect(source)) return Urlencoded.unpack(source);
-      throw 'Not matched';
+      throw '原因：未匹配到有效的 URL 编码格式';
     },
     p_a_c_k_e_r: () => {
       self.importScripts('{{ "third_party/js-beautify/unpackers/p_a_c_k_e_r_unpacker.js" | relative_url }}');
       if (P_A_C_K_E_R.detect(source)) return P_A_C_K_E_R.unpack(source);
-      throw 'Not matched';
+      throw '原因：未匹配到有效的 P_A_C_K_E_R 格式';
     },
     javascriptobfuscator: () => {
       self.importScripts('{{ "third_party/js-beautify/unpackers/javascriptobfuscator_unpacker.js" | relative_url }}');
       if (JavascriptObfuscator.detect(source)) return JavascriptObfuscator.unpack(source);
-      throw 'Not matched';
+      throw '原因：未匹配到有效的 JavascriptObfuscator 格式';
     },
     myobfuscate: () => {
       self.importScripts('{{ "third_party/js-beautify/unpackers/myobfuscate_unpacker.js" | relative_url }}');
       if (MyObfuscate.detect(source)) return MyObfuscate.unpack(source);
-      throw 'Not matched';
+      throw '原因：未匹配到有效的 MyObfuscate 格式';
     },
     wiseeval: () => {
       self.importScripts('{{ "third_party/NotSoWise/unpacker.js" | relative_url }}');
@@ -86,7 +89,7 @@ self.addEventListener('message', (e) => {
   try {
     source = methods[packer]();
   } catch (err) {
-    throw new Error(err);
+    throw new Error(`原因：解码时发生错误：方法 [${packer}] 失败，原因：${err}`);
   }
 
   self.postMessage(source);
